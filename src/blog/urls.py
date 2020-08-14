79967a5b7e21b4a2c90af301f9e4b70c521aab98
nicolasbancel <nicolas.bancel@gmail.com>
2020-08-14 17:00:24 +0200

    cours 40

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from .views import (ArticleListView)
+from .views import (ArticleCreateView,
+                    ArticleListView,
+                    ArticleDetailView,
+                    ArticleUpdateView,
+                    ArticleDeleteView)
 
 app_name = 'articles'
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article-list')
+    path('', ArticleListView.as_view(), name='article-list'),
+    path('create/', ArticleCreateView.as_view(), name='article-create'),
+    path('update/<int:id>', ArticleUpdateView.as_view(), name='article-update'),
+    path('delete/<int:id>', ArticleDeleteView.as_view(), name='article-delete'),
+    # If we use the ID, getting error message : Generic detail view ArticleDetailView must be called with either an object pk or a slug in the URLconf.
+    #path('<int:id>', ArticleDetailView.as_view(), name='article-detail')
+    path('<int:id>', ArticleDetailView.as_view(), name='article-detail')
 
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -12,7 +12,57 @@ from django.views.generic import (
 )
 
 from .models import Article
+from .forms import ArticleModelForm
+
+
+class ArticleCreateView(CreateView):
+    template_name = 'articles/article_create.html'
+    form_class = ArticleModelForm
+    queryset = Article.objects.all() # Will connect to the template : <app_name>/<model_name>_list.html : because inherits from ListView
+
+    def form_valid(self, form):
+        # Prints in the terminal the content of the form in a dictionnary
+        #print(form.cleaned_data)
+        return super().form_valid(form)
+
 
 class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
     queryset = Article.objects.all() # Will connect to the template : <app_name>/<model_name>_list.html : because inherits from ListView
+
+class ArticleDetailView(DetailView):
+    template_name = 'articles/article_detail.html'
+    # No need for the queryset : this limits the choices available for the detail vie
+    queryset = Article.objects.all()
+    # Example : queryset could limit the ids to be greater than 1 // when all() : it's useless
+    #queryset = Article.objects.filter(id__gt=1)
+
+    def get_object(self):
+        # Those are the kwargs of
+        id_ = self.kwargs.get('id')
+        return get_object_or_404(Article, id=id_)
+
+class ArticleUpdateView(UpdateView):
+    template_name = 'articles/article_create.html'
+    form_class = ArticleModelForm
+    queryset = Article.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get('id')
+        return get_object_or_404(Article, id=id_)
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+class ArticleDeleteView(DeleteView):
+    template_name = 'articles/article_delete.html'
+    #queryset = Article.objects.all()
+
+    def get_object(self):
+        # Those are the kwargs of
+        id_ = self.kwargs.get('id')
+        return get_object_or_404(Article, id=id_)
+
+    def get_success_url(self):
+        return reverse('articles:article-list')
